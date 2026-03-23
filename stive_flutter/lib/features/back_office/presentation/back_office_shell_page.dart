ca29a4c43fd6b703ed8774de3009d9f37a230159
Tom Vaillant <tom.vaillant@etu.unicaen.fr>
2026-03-23 22:53:36 +0100

    feat: ajout de la page BackOffice pour la gestion des articles, familles, fournisseurs, clients et commandes

---
+++ stive_flutter/lib/features/back_office/presentation/back_office_shell_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import 'pages/articles_page.dart';
+import 'pages/clients_page.dart';
+import 'pages/commandes_clients_page.dart';
+import 'pages/commandes_fournisseurs_page.dart';
+import 'pages/familles_page.dart';
+import 'pages/fournisseurs_page.dart';
+import 'pages/inventaires_page.dart';
+
+class BackOfficeShellPage extends StatefulWidget {
+  const BackOfficeShellPage({super.key});
+
+  @override
+  State<BackOfficeShellPage> createState() => _BackOfficeShellPageState();
+}
+
+class _BackOfficeShellPageState extends State<BackOfficeShellPage> {
+  int _index = 0;
+
+  final _items = const [
+    _NavItem('Articles', Icons.inventory_2_outlined),
+    _NavItem('Familles', Icons.local_bar_outlined),
+    _NavItem('Fournisseurs', Icons.store_outlined),
+    _NavItem('Clients', Icons.people_alt_outlined),
+    _NavItem('Cmd clients', Icons.receipt_long_outlined),
+    _NavItem('Cmd fourn.', Icons.local_shipping_outlined),
+    _NavItem('Inventaires', Icons.fact_check_outlined),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final pages = [
+      const ArticlesPage(),
+      const FamillesPage(),
+      const FournisseursPage(),
+      const ClientsPage(),
+      const CommandesClientsPage(),
+      const CommandesFournisseursPage(),
+      const InventairesPage(),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Back-office • ${_items[_index].label}')),
+      drawer: Drawer(
+        child: SafeArea(
+          child: ListView(
+            children: [
+              const ListTile(
+                title: Text('NEGOSUD'),
+                subtitle: Text('Gestion entrepot'),
+              ),
+              const Divider(),
+              for (var i = 0; i < _items.length; i++)
+                ListTile(
+                  leading: Icon(_items[i].icon),
+                  title: Text(_items[i].label),
+                  selected: i == _index,
+                  onTap: () {
+                    setState(() => _index = i);
+                    Navigator.pop(context);
+                  },
+                ),
+            ],
+          ),
+        ),
+      ),
+      body: pages[_index],
+    );
+  }
+}
+
+class _NavItem {
+  const _NavItem(this.label, this.icon);
+
+  final String label;
+  final IconData icon;
+}
